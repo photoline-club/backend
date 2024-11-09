103dae80c8c171fad3e50746c4d7554e25b06e5c
Patrick Jones <git@patrickjones.uk>
2024-11-09 16:18:46 +0000

    database structure

---
+++ database/database.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/photoline-club/backend/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type DBConfig struct {
+	User     string
+	Password string
+	Host     string
+	Port     string
+	DBName   string
+}
+
+func (c *DBConfig) ConnectionString() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		c.User,
+		c.Password,
+		c.Host,
+		c.Port,
+		c.DBName,
+	)
+}
+
+func InitialiseDB(cfg DBConfig) *gorm.DB {
+	fmt.Printf("cfg: %v\n", cfg)
+	fmt.Printf("cfg.ConnectionString(): %v\n", cfg.ConnectionString())
+	db, err := gorm.Open(mysql.Open(cfg.ConnectionString()), &gorm.Config{})
+	if err != nil {
+		panic("Unable to connect to database")
+	}
+	db.AutoMigrate(&models.User{})
+	db.AutoMigrate(&models.FriendLink{})
+	db.AutoMigrate(&models.Event{})
+	db.AutoMigrate(&models.EventAsset{})
+	db.AutoMigrate(&models.EventParticipant{})
+	return db
+}
--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/photoline-club/backend/config"
+	"github.com/photoline-club/backend/database"
 )
 
 func main() {
-    r := gin.Default()
-    r.GET("/", func(ctx *gin.Context) {
-        ctx.String(http.StatusOK, "online")
-    })
-    r.Run("0.0.0.0:8080")
+	database.InitialiseDB(config.GetDBConfig())
+
+	r := gin.Default()
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "online")
+	})
+	r.Run("0.0.0.0:8080")
 }
