0cb2db980fe2dc1ce58f00ceaa4579718e30f095
Patrick Jones <git@patrickjones.uk>
2024-11-09 15:07:34 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+    r := gin.Default()
+    r.GET("/", func(ctx *gin.Context) {
+        ctx.String(http.StatusOK, "online")
+    })
+    r.Run("0.0.0.0:8080")
+}
