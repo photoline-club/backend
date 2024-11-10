1a89ba52533d0131bbce33fd094f3ba5b134f250
harrrrion <harrisonrigby@icloud.com>
2024-11-10 04:01:55 +0000

    final chanegs to images file

--- routes/images.go
+++ routes/images.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"strings"
 	"strconv"
+	"errors"
 )
 
 type ImageReq struct{
@@ -25,8 +26,12 @@ func GetImages(context *gin.Context){ // the context conatins the infor for the 
 	user := middleware.GetUser(context)
 
 	var images [] models.EventAsset
-	db.Model(&models.EventAsset{}).Where("event_id = ? AND (NOT private OR (user_ID = ?))", EventID, user.ID).Find(&images)
-
+	if !errors.Is(db.Model(&models.EventAsset{}).Where("event_id = ? AND (NOT private OR (user_ID = ?))", EventID, user.ID).Find(&images)){
+	context.IndentedJSON(http.StatusOK, images)
+	}else{
+		context.IndentedJSON(http.StatusBadRequest, nil)
+	}
+	
 	context.IndentedJSON(http.StatusOK, images)
 	
 }
@@ -50,6 +55,7 @@ func AddImages(context *gin.Context){
 		id_string := context.Param("id")
 		id, err := strconv.ParseUint(id_string, 10, 64)
 		private, _ := context.GetQuery("private")
+	filenames_out := make([]string, 0)
 	if err != nil{
 		context.IndentedJSON(http.StatusBadRequest, gin.H{})
 		return 
@@ -73,11 +79,11 @@ func AddImages(context *gin.Context){
 		}
 			db.Save(&asset)
 			
-			context.SaveUploadedFile(file, filename)
-
+			context.SaveUploadedFile(file, filename) //need to error cehck this
+			filenames_out = append(filenames_out, filename)
 		}
 
-	context.IndentedJSON(http.StatusCreated, nil)
+	context.IndentedJSON(http.StatusCreated, filenames_out) // is this good???
 }
 
 func SetUpImagesRoutes(router *gin.RouterGroup){
